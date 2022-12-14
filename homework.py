31490fd63a6d315f7e7e3f3f93d25873da646a6f
Стас Басов <stas.basov@whatever.com>
2022-12-14 15:56:23 +0300

    комент1

--- homework.py
+++ homework.py
@@ -1,24 +1,23 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self,
-                 training_type,
-                 duration,
-                 distance,
-                 speed,
-                 calories,
-                 ) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    message: str = ('Тип тренировки: {training_type}; '
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        """Вывод совокупных результатов пользователю"""
+        return self.message.format(**asdict(self))
 
 
 class Training:
@@ -29,7 +28,6 @@ class Training:
     # Константа для перевода значений из метров в километры.
     MIN_IN_H: float = 60
     CM_IN_M: float = 100
-    KMH_IN_MS: float = 0.278
 
     def __init__(self,
                  action: int,
@@ -50,11 +48,11 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise NotImplementedError('<текст сообщения>')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.__class__.__name__,
+        return InfoMessage(type(self).__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
@@ -77,7 +75,6 @@ class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     CALORIES_WEIGHT_MULTIPLIER: float = 0.035
     CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
-    CM_IN_M: float = 100
     KMH_IN_MSEC: float = 0.278
 
     def __init__(self, action, duration,
@@ -95,10 +92,9 @@ class SportsWalking(Training):
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP = 1.38
-    # Расстояние, которое спортсмен преодолевает за один шаг или гребок.
-    CALORIES_MEAN_SPEED_MULTIPLIER = 1.1
-    CALORIES_MEAN_SPEED_SHIFT = 2
+    LEN_STEP: float = 1.38
+    CALORIES_MEAN_SPEED_MULTIPLIER: float = 1.1
+    CALORIES_MEAN_SPEED_SHIFT: float = 2
 
     def __init__(self,
                  action: int,
@@ -111,31 +107,30 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_spent_calories(self) -> float:
-        result = ((self.get_mean_speed() + self.CALORIES_MEAN_SPEED_MULTIPLIER)
-                  * self.CALORIES_MEAN_SPEED_SHIFT
-                  * self.weight * self.duration)
-        return result
+        return ((self.get_mean_speed() + self.CALORIES_MEAN_SPEED_MULTIPLIER)
+                * self.CALORIES_MEAN_SPEED_SHIFT
+                * self.weight * self.duration)
 
     def get_mean_speed(self) -> float:
-        result = (self.length_pool * self.count_pool
-                  / self.M_IN_KM / self.duration)
-        return result
+        return (self.length_pool * self.count_pool
+                / self.M_IN_KM / self.duration)
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str, data: list[float]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    TOA = {'RUN': Running,
-           'WLK': SportsWalking,
-           'SWM': Swimming}
-    # TOA = type of activity.
-    Object_TOA: Training = TOA[workout_type](*data)
-    return Object_TOA
+    workout_types = {'RUN': Running,
+                     'WLK': SportsWalking,
+                     'SWM': Swimming}
+
+    if workout_type in workout_types:
+        return workout_types[workout_type](*data)
+    else:
+        raise KeyError('<key_name>')
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info: InfoMessage = training.show_training_info()
-    print(info.get_message())
+    print(training.show_training_info().get_message())
 
 
 if __name__ == '__main__':
